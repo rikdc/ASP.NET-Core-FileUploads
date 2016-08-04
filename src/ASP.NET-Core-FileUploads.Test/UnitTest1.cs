642f38b605f3cb238ef0cf8517e26e3c3eadbd4c
Richard Claydon <richard@ugrworks.com>
2016-08-04 17:37:41 -0400

    Adding unit tests.

---
+++ src/ASP.NET-Core-FileUploads.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASP.NET_Core_FileUploads.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
