598ef524d4731e961d541c9e084d70e3fd011d6b
Richard Claydon <richard@ugrworks.com>
2016-08-04 15:03:58 -0400

    Initial import of a default project and gitignore.

---
+++ src/ASP.NET-Core-FileUploads/Controllers/DocumentsController.cs
@@ -0,0 +1,34 @@
+﻿using ASP.NET_Core_FileUploads.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_FileUploads.Controllers
+{
+    public class DocumentsController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] DocumentInput upload)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var file = upload.File;
+            var parsedContentDisposition =
+                ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+
+            var filename = Path.Combine("uploads", parsedContentDisposition.FileName.Trim('"'));
+
+            var cancellationToken = default(CancellationToken);
+
+            using (var fileStream = new FileStream(filename, FileMode.Create))
+            {
+                var inputStream = upload.File.OpenReadStream();
+                await inputStream.CopyToAsync(fileStream, 81920, cancellationToken);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ src/ASP.NET-Core-FileUploads/Models/Dto/DocumentInput.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.NET_Core_FileUploads.Models.Dto
+{
+    public class DocumentInput
+    {
+        [Required]
+        [MinLength(4)]
+        public string Name { get; set; }
+
+        [Required]
+        [FileExtensions(Extensions = "jpg,jpeg,doc,docx,pdf")]
+        public IFormFile File { get; set; }
+    }
+}
