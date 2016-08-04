54e92acbdaa325dba1869397af5ca68f34f7dcd2
Richard Claydon <richard@ugrworks.com>
2016-08-04 15:25:35 -0400

    Creating the DocumentInput Dto.

--- src/ASP.NET-Core-FileUploads/wwwroot/js/site.js
+++ src/ASP.NET-Core-FileUploads/wwwroot/js/site.js
@@ -1,1 +1,25 @@
-﻿// Write your Javascript code.
+﻿$(document).ready(function () {
+    $("[data-form-uploader]").submit(function (evt) {
+        evt.preventDefault();
+
+        var fileUpload = $("#file").get(0);
+        var files = fileUpload.files;
+        var data = new FormData();
+        for (var i = 0; i < files.length ; i++) {
+            data.append(files[i].name, files[i]);
+        }
+        $.ajax({
+            type: "POST",
+            url: "/api/documents/",
+            contentType: false,
+            processData: false,
+            data: data,
+            success: function (message) {
+                alert(message);
+            },
+            error: function () {
+                alert("There was error uploading files!");
+            }
+        });
+    });
+});
